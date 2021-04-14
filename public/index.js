eadcba6b6ebb60cfbd7438a6683f83e1583aef55
vshulman25 <victoriashulman25@gmail.com>
2021-04-14 07:49:03 -0400

    deploying issues with transaction reduce not being a function

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  const total = transactions.reduce((total, t) => {
+  let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
 
