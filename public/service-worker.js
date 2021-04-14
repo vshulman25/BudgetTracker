827a01e2cd2d09fed40b31d2a7f444495263a72d
vshulman25 <victoriashulman25@gmail.com>
2021-04-14 14:23:10 -0400

    added activate to service worker to fix error

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,17 +3,43 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/db.js',
     '/index.js',
-    'style.css'
+    'style.css',
 ];
 
 const PRECACHE = 'cache-v1';
+const RUNTIME = 'runtime';
 const DATA_CACHE = 'data-cache-v1';
 
-self.addEventListener("install", event => {
-    event.waitUntil(caches.open(PRECACHE).then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
-    }))
-})
+// self.addEventListener("install", event => {
+//     event.waitUntil(caches.open(PRECACHE).then(cache => {
+//         return cache.addAll(FILES_TO_CACHE).then(self.skipWaiting());
+//     }))
+// })
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cachesToDelete) => {
+           Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
 
 self.addEventListener("fetch", event => {
     if (event.request.url.includes("/api/")) {
